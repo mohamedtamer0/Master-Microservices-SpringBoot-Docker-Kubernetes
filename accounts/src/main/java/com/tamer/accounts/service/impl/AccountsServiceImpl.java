0901e8391a863a4d1e352b3335c93fa5d8ef514c
mohamedtamer0 <mt4452242@gmail.com>
2024-08-08 17:12:29 +0300

    Update

--- accounts/src/main/java/com/tamer/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/tamer/accounts/service/impl/AccountsServiceImpl.java
@@ -15,6 +15,7 @@ import com.tamer.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 
@@ -36,6 +37,8 @@ public class AccountsServiceImpl  implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
                     +customerDto.getMobileNumber());
         }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -52,6 +55,8 @@ public class AccountsServiceImpl  implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now());
+        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
